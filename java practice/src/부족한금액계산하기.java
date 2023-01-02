8f48beb8e0e0624259ec06fc52a3f43841a6f814
seokgyu choi <tommyne99@naver.com>
2023-01-02 15:35:44 +0900

    20230102

--- java practice/src/부족한금액계산하기.java
+++ java practice/src/부족한금액계산하기.java
@@ -1,10 +1,13 @@
-class Solution {
-    public long solution(int price, int money, int count) {
-        long totalprice =0;
-        
-        for(int i=1; i<=count; i++){
-            totalprice += price *i;
-        }
-        return (money - totalprice) >= 0 ? 0 : (money - totalprice) * -1;
-    }
+
+public class 부족한금액계산하기 {
+	class Solution {
+	    public long solution(int price, int money, int count) {
+	        long totalprice =0;
+	        
+	        for(int i=1; i<=count; i++){
+	            totalprice += price *i;
+	        }
+	        return (money - totalprice) >= 0 ? 0 : (money - totalprice) * -1;
+	    }
+	}
 }
